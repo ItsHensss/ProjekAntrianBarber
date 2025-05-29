7e4dd534c20396701d55d89057f0f72587b0d76d
Arima1213 <arima3992@gmail.com>
2025-05-29 11:45:49 +0700

    refactor: Remove unused imports in QueueResource and add missing BelongsTo import in Lokasi model

--- app/Filament/Resources/QueueResource.php
+++ app/Filament/Resources/QueueResource.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\QueueResource\Pages;
-use App\Filament\Resources\QueueResource\RelationManagers;
 use App\Models\Queue;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -11,7 +10,6 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Auth;
 
 class QueueResource extends Resource
