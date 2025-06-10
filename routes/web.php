9ac708f8ea8c7c162e6a96601e588515e95ae479
Arima1213 <arima3992@gmail.com>
2025-06-10 22:39:21 +0700

    fix: Add missing import for TransaksiExportController in routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\galleryController;
 use App\Http\Controllers\nomorAntrian;
 use App\Http\Controllers\QueueValidationController;
 use App\Http\Controllers\SummaryExportController;
+use App\Http\Controllers\TransaksiExportController;
 use Illuminate\Support\Facades\Crypt;
 
 Route::get('/validasi-antrian/{queue}', [QueueValidationController::class, 'show'])->name('validasi.antrian.show');
