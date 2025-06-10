a9f20e8e6570c2d2366c4c3d20ce88b80b02f183
Arima1213 <arima3992@gmail.com>
2025-06-10 21:18:55 +0700

    feat: Add SummaryExportController for PDF export of summary reports

---
+++ app/Http/Controllers/SummaryExportController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Queue;
+use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
+use Carbon\Carbon;
+
+class SummaryExportController extends Controller
+{
+    public function export(Request $request)
+    {
+        $from = Carbon::parse($request->input('from'))->startOfDay();
+        $until = Carbon::parse($request->input('until'))->endOfDay();
+
+        $dates = [];
+        $period = new \DatePeriod($from, new \DateInterval('P1D'), $until->copy()->addDay());
+        foreach ($period as $date) {
+            $dates[] = $date->format('Y-m-d');
+        }
+
+        $queues = Queue::with(['user', 'produk'])
+            ->whereBetween('booking_date', [$from->format('Y-m-d'), $until->format('Y-m-d')])
+            ->get();
+
+        $summary = [];
+
+        foreach ($queues as $queue) {
+            $name = $queue->user->name ?? '-';
+            $layanan = $queue->produk->judul ?? '-';
+            $date = $queue->booking_date;
+
+            if (!isset($summary[$name])) {
+                $summary[$name] = [];
+            }
+
+            if (!isset($summary[$name][$layanan])) {
+                $summary[$name][$layanan] = array_fill_keys($dates, 0);
+            }
+
+            $summary[$name][$layanan][$date]++;
+        }
+
+        foreach ($summary as $name => $layanans) {
+            foreach ($layanans as $layanan => $tanggal) {
+                $summary[$name][$layanan]['total'] = array_sum($tanggal);
+            }
+        }
+
+        $data = [
+            'from' => $from->format('Y-m-d'),
+            'until' => $until->format('Y-m-d'),
+            'dates' => $dates,
+            'summary' => $summary,
+        ];
+
+        $pdf = FacadePdf::loadView('exports.summary', $data)->setPaper('A4', 'landscape');
+        return $pdf->stream("Laporan_Ringkasan_{$from->format('Ymd')}_{$until->format('Ymd')}.pdf");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('/antrian/today/json/{id}', [NomorAntrian::class, 'jsonToday'])->name
 Route::get('/antrian/{queue}/print', [NomorAntrian::class, 'print'])->name('antrian.print');
 
 Route::get('/export-transaksi', [ExportController::class, 'export'])->name('export.transaksi');
+Route::get('/summary/export/pdf', [SummaryExportController::class, 'export'])->name('summary.export.pdf');
