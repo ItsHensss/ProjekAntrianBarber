ece23f4d4447eb6d91bac12115a5513632683acc
Arima1213 <arima3992@gmail.com>
2025-06-11 19:26:57 +0700

    fix: Simplify value display in summary report table

--- resources/views/filament/resources/summary-resource/pages/summary-report.blade.php
+++ resources/views/filament/resources/summary-resource/pages/summary-report.blade.php
@@ -14,55 +14,57 @@
 					PDF
 				</x-filament::button>
 			</div>
-
 		</div>
 	</form>
+
 	@php
 		$from = $this->from;
 		$until = $this->until;
-	@endphp
-
-	<h2 class="mb-4 text-lg font-bold text-gray-900 dark:text-gray-100">Laporan Ringkasan</h2>
-
-	@php
 		$today = \Carbon\Carbon::today();
 	@endphp
-
-	<table class="min-w-full table-auto border-collapse border border-gray-300 dark:border-gray-700">
-		<thead>
-			<tr class="bg-gray-100 dark:bg-gray-800">
-				<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Nama Karyawan</th>
-				<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Jenis Layanan</th>
-				@foreach ($data['dates'] as $date)
-					<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">{{ $date }}</th>
+	<div class="overflow-x-auto">
+		<table class="min-w-full table-auto border-collapse border border-gray-300 dark:border-gray-700">
+			<thead>
+				<tr class="bg-gray-100 dark:bg-gray-800">
+					<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Nama Karyawan
+					</th>
+					<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Jenis Layanan
+					</th>
+					@foreach ($data['dates'] as $date)
+						<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">
+							{{ $date }}</th>
+					@endforeach
+					<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Total</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($data['summary'] as $karyawan => $layanans)
+					@foreach ($layanans as $layanan => $items)
+						<tr class="bg-white dark:bg-gray-900">
+							<td class="border px-4 py-2 text-gray-800 dark:border-gray-700 dark:text-gray-200">
+								{{ $karyawan }}</td>
+							<td class="border px-4 py-2 text-gray-800 dark:border-gray-700 dark:text-gray-200">
+								{{ $layanan }}</td>
+							@foreach ($data['dates'] as $date)
+								@php
+									$value = $items[$date] ?? 0;
+									$isPast = \Carbon\Carbon::parse($date)->lessThanOrEqualTo($today);
+								@endphp
+								<td
+									class="{{ $value == 0 && $isPast ? 'bg-red-100 text-red-700 font-bold' : 'text-gray-800 dark:text-gray-200' }} border px-4 py-2 text-center dark:border-gray-700">
+									@if ($value == 0 && $isPast)
+										-
+									@else
+										{{ $value }}
+									@endif
+								</td>
+							@endforeach
+							<td class="border px-4 py-2 text-center font-semibold text-gray-900 dark:border-gray-700 dark:text-gray-100">
+								{{ $items['total'] }}</td>
+						</tr>
+					@endforeach
 				@endforeach
-				<th class="border px-4 py-2 text-gray-900 dark:border-gray-700 dark:text-gray-100">Total</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($data['summary'] as $karyawan => $layanans)
-				@foreach ($layanans as $layanan => $items)
-					<tr class="bg-white dark:bg-gray-900">
-						<td class="border px-4 py-2 text-gray-800 dark:border-gray-700 dark:text-gray-200">{{ $karyawan }}</td>
-						<td class="border px-4 py-2 text-gray-800 dark:border-gray-700 dark:text-gray-200">{{ $layanan }}</td>
-						@foreach ($data['dates'] as $date)
-							@php
-								$value = $items[$date] ?? 0;
-								$isPast = \Carbon\Carbon::parse($date)->lessThanOrEqualTo($today);
-							@endphp
-							<td
-								class="{{ $value == 0 && $isPast ? 'bg-red-100 text-red-700 font-bold' : 'text-gray-800 dark:text-gray-200' }} border px-4 py-2 text-center dark:border-gray-700">
-								@if ($value == 0 && $isPast)
-									cuti
-								@else
-									{{ $value }}
-								@endif
-							</td>
-						@endforeach
-						<td class="border px-4 py-2 text-center font-semibold text-gray-900 dark:border-gray-700 dark:text-gray-100">{{ $items['total'] }}</td>
-					</tr>
-				@endforeach
-			@endforeach
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	</div>
 </x-filament::page>
