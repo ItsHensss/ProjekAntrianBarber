cde7056007d7e7ad1d5dc2bdc03c51aa1e7fd6fd
Arima1213 <arima3992@gmail.com>
2025-06-12 13:49:40 +0700

    fix: Increase queue seeder limit to 10000 and streamline user assignment

--- database/seeders/QueueSeeder.php
+++ database/seeders/QueueSeeder.php
@@ -14,16 +14,17 @@ class QueueSeeder extends Seeder
     {
         $customers = Customer::pluck('id')->toArray();
         $produkIds = [1, 2, 3]; // Sesuai produk di cabang utama
+        $userIds = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-        for ($i = 1; $i <= 5000; $i++) {
+        for ($i = 1; $i <= 10000; $i++) {
             Queue::create([
                 'customer_id' => Arr::random($customers),
                 'produk_id' => Arr::random($produkIds),
                 'tenant_id' => 1, // cabang utama
+                'user_id' => Arr::random($userIds), // chapster yang melayani, bisa null
                 'nomor_antrian' => $i,
                 'status' => Arr::random(['menunggu', 'selesai', 'batal']),
-                'is_validated' => (bool)rand(0, 1),
-                'requested_chapster_id' => Arr::random(['dani', 'umum']),
+                'is_validated' => 1,
                 'booking_date' => Carbon::now()->subDays(rand(0, 30)),
             ]);
         }
